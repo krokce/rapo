3900158f3cc13cea44f6b4659059beecef21aa3d
Kostadin Taneski <kosta@taneski.com>
2026-09-20 09:45:53 +0200

    Drop the packaging metadata of the PyPI release
    - Remove the long description, classifiers and home page from setup.py
    - Remove the build/ and egg-info of v0.6.15 from the working tree
    - Leave the install itself unchanged

--- setup.py
+++ setup.py
@@ -9,10 +9,7 @@ version = rapo.__version__
 author = rapo.__author__
 author_email = rapo.__email__
 description = rapo.__doc__
-long_description = open('README.md', 'r').read()
-long_description_content_type = 'text/markdown'
 license = rapo.__license__
-url = f'https://github.com/t3eHawk/{name}'
 python_requires = '>=3.10'
 install_requires = open('requirements.txt').read().splitlines()
 packages = setuptools.find_packages()
@@ -23,13 +20,6 @@ package_data = {
         'algorithms/**/*'
     ]
 }
-classifiers = [
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11',
-    'Programming Language :: Python :: 3.12',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent'
-]
 entry_points = {
     'console_scripts': [
         'rapo-scheduler=rapo.cli:run_scheduler',
@@ -43,13 +33,9 @@ setuptools.setup(name=name,
                  author=author,
                  author_email=author_email,
                  description=description,
-                 long_description=long_description,
-                 long_description_content_type=long_description_content_type,
                  license=license,
-                 url=url,
                  python_requires=python_requires,
                  install_requires=install_requires,
                  packages=packages,
                  package_data=package_data,
-                 classifiers=classifiers,
                  entry_points=entry_points)
